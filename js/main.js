1fa1126126a1cff69e713bde9acc7b05de488148
luongvanlinh3003 <luongvanlinh12@gmail.com>
2023-08-19 23:33:27 +0700

    random 9 pairs of color and bind colors to li elements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
+import { getRandomColorPairs } from './utils.js'
+import { getColorElementList, getColorListElement } from './selectors.js'
 
 // Global variables
 let selections = []
@@ -10,3 +12,41 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
+
+function handleColorClick(liELement) {
+  if (!liELement) return
+
+  liELement.classList.add('active')
+}
+
+function initColors() {
+  // random 8 pairs of colors
+  const colorList = getRandomColorPairs(PAIRS_COUNT)
+
+  // bind to li > div.overlay
+  const liList = getColorElementList()
+
+  liList.forEach((liELement, index) => {
+    const overlayElement = liELement.querySelector('.overlay')
+
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
+}
+
+function attachEventForColorList() {
+  const ulElement = getColorListElement()
+
+  if (!ulElement) return
+
+  // event delegation
+  ulElement.addEventListener('click', (event) => {
+    handleColorClick(event.target)
+  })
+}
+
+// main
+;(() => {
+  initColors()
+
+  attachEventForColorList()
+})()
