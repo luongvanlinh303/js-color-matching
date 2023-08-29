9de75e80669ae2514b4ddccb221bcf01937aeb55
luongvanlinh3003 <luongvanlinh12@gmail.com>
2023-08-29 23:12:44 +0700

    handle integrate game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
 import { getRandomColorPairs } from './utils.js'
-import { getColorElementList, getColorListElement } from './selectors.js'
+import { getColorElementList, getColorListElement, getInActiveColorList } from './selectors.js'
 
 // Global variables
 let selections = []
-let gameState = GAME_STATUS.PLAYING
+let gameStatus = GAME_STATUS.PLAYING
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -13,10 +13,53 @@ let gameState = GAME_STATUS.PLAYING
 // 4. Add timer
 // 5. Handle replay click
 
+// handleColorClick 1
+// handleColorClick 2
+// handleColorClick 3
+// setTimeout 2 --> reset selections
+// setTimeout 3 --> errors here
 function handleColorClick(liELement) {
-  if (!liELement) return
+  const shouldBlockCick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameStatus)
 
+  if (!liELement || shouldBlockCick) return
+
+  // show clicked cell to selections
   liELement.classList.add('active')
+
+  // save clicked cell to selections
+  selections.push(liELement)
+  if (selections.length < 2) return
+
+  // check match
+  const firstColor = selections[0].dataset.color
+  const secondColor = selections[1].dataset.color
+  const isMatch = firstColor === secondColor
+
+  if (isMatch) {
+    // check win
+    const isWin = getInActiveColorList().length === 0
+    if (isWin) {
+      // show replay
+      // show You WIN
+    }
+
+    selections = []
+    return
+  }
+
+  // in case of not match
+  // remove active class for 2 li elements
+  gameStatus = GAME_STATUS.BLOCKING
+
+  setTimeout(() => {
+    selections[0].classList.remove('active')
+    selections[1].classList.remove('active')
+
+    // reset selections for the next turn
+    selections = []
+
+    gameStatus = GAME_STATUS.PLAYING
+  }, 500)
 }
 
 function initColors() {
@@ -27,6 +70,8 @@ function initColors() {
   const liList = getColorElementList()
 
   liList.forEach((liELement, index) => {
+    liELement.dataset.color = colorList[index]
+
     const overlayElement = liELement.querySelector('.overlay')
 
     if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
@@ -40,6 +85,7 @@ function attachEventForColorList() {
 
   // event delegation
   ulElement.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return
     handleColorClick(event.target)
   })
 }
